2a296f7d843c6b4931465ca277ce0c0e6f046367
Alex Diaz <alex@akdev.xyz>
2023-04-15 13:44:13 -0400

    feat: add dotfiles image with dotfiles

--- images/openvpn/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat > "${OPENVPN_PASSWD_FILE}" <<EOF
-${OPENVPN_USER}
-${OPENVPN_PASS}
-EOF
-
-exec openvpn --config "${OPENVPN_CONFIG_DIR}/${OPENVPN_PROFILE}.ovpn" \
-             --auth-user-pass "${OPENVPN_PASSWD_FILE}"
