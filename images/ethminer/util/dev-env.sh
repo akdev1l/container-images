cfe779235f79afbd117cb5aaf3bceecf44dfd86a
Alex Diaz <alex@akdev.xyz>
2026-03-08 15:13:11 -0400

    feat: add imgctl script

--- images/ethminer/util/dev-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -LJ "${ETHMINER_RELEASE_URL}" -o /tmp/ethminer.tar.gz
-
-(
-    cd /opt
-    tar xvf /tmp/ethminer.tar.gz
-)
--- images/sddm-container/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-echo "${AKDEV_PASSWORD}" | sudo passwd akdev --stdin
-
-exec /sbin/init
